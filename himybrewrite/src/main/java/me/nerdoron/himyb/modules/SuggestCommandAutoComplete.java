dc83c84e77a365b104f6e4a931c4445b671e4466
goseale <2010ale2@gmail.com>
2022-06-06 14:57:16 -0300

    Changes to birthday and compresed code

--- himybrewrite/src/main/java/me/nerdoron/himyb/modules/SuggestCommandAutoComplete.java
+++ himybrewrite/src/main/java/me/nerdoron/himyb/modules/SuggestCommandAutoComplete.java
@@ -1,5 +1,6 @@
 package me.nerdoron.himyb.modules;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -9,18 +10,20 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.Command;
 
 public class SuggestCommandAutoComplete extends ListenerAdapter {
-    private String[] types = new String[] { "server", "video", "bot", "chain" };
+    ArrayList<String> types = new ArrayList<>(List.of(new String[]{"server", "video", "bot", "chain"}));
 
     @Override
     public void onCommandAutoCompleteInteraction(CommandAutoCompleteInteractionEvent event) {
 
         if (event.getName().equals("suggest") && event.getFocusedOption().getName().equals("type")) {
-            List<Command.Choice> options = Stream.of(types)
-                    .filter(type -> type.startsWith(event.getFocusedOption().getValue()))
-                    .map(type -> new Command.Choice(type, type))
-                    .collect(Collectors.toList());
+            ArrayList<Command.Choice> options = new ArrayList<>();
+            for (String type : types) {
+                options.add(new Command.Choice(type, type));
+            }
+
             event.replyChoices(options).queue();
         }
+
     }
 
 }
