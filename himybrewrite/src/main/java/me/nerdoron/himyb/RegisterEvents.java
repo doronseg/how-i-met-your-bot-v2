eae122d04ce16ca68974841c103d4d616c0cbd02
goseale <2010ale2@gmail.com>
2022-06-22 13:12:24 -0300

    Selfpromo sicky msg - Needs ID change Transcript base

--- himybrewrite/src/main/java/me/nerdoron/himyb/RegisterEvents.java
+++ himybrewrite/src/main/java/me/nerdoron/himyb/RegisterEvents.java
@@ -17,6 +17,7 @@ import me.nerdoron.himyb.modules.chainchannel.ChainEditing;
 import me.nerdoron.himyb.modules.counting.CountingChannelHandler;
 import me.nerdoron.himyb.modules.counting.CountingEditing;
 import me.nerdoron.himyb.modules.help.HelpButtonHandler;
+import me.nerdoron.himyb.modules.selfpromo.SelfPromoHandler;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.ChannelType;
@@ -111,6 +112,7 @@ public class RegisterEvents {
                 jda.addEventListener(new EventManagerApplicationHandler());
                 jda.addEventListener(new BirthdayAutoComplete());
                 jda.addEventListener(new BirthdayFunction());
+                jda.addEventListener(new SelfPromoHandler());
 
         }
 
