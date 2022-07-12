104d957017f6a726697965da6c6e2586d5582f20
nerdoron <doron.segev1803@gmail.com>
2022-07-12 21:33:19 +0300

    remove unused imports

--- himybrewrite/src/main/java/me/nerdoron/himyb/commands/SlashCommand.java
+++ himybrewrite/src/main/java/me/nerdoron/himyb/commands/SlashCommand.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.internal.interactions.CommandDataImpl;
 
 public abstract class SlashCommand {
 
--- himybrewrite/src/main/java/me/nerdoron/himyb/commands/currencycommands/BankCommand.java
+++ himybrewrite/src/main/java/me/nerdoron/himyb/commands/currencycommands/BankCommand.java
@@ -11,7 +11,6 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.internal.interactions.CommandDataImpl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
